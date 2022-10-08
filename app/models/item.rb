3afb4368ca32e7f23eeedbb4db71ca069a8d3782
shimitar <8bi2sz6723@gmail.com>
2022-10-08 19:43:33 +0900

    バリデーション　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,7 @@ class Item < ApplicationRecord
   
   validates :image, presence: true
   validates :name,             presence: true
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 }
-  end
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 },presence: true
   validates :item_text,        presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"},presence: true
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"},presence: true
