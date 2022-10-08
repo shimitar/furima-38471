f47bfb2ebe5af855a8de8665f5d3913664cba3b1
shimitar <8bi2sz6723@gmail.com>
2022-10-08 16:15:24 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -9,7 +10,9 @@ class Item < ApplicationRecord
   
   validates :image, presence: true
   validates :name,             presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 },presence: true
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 }
+  end
   validates :item_text,        presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"},presence: true
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"},presence: true
